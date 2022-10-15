32247bae96744af719fb54a44f6e1404e578518b
AbyPaul9 <akandeabayomipaul@gmail.com>
2022-10-15 09:14:44 -0700

    Task 10

--- models/place.py
+++ models/place.py
@@ -65,4 +65,4 @@ class Place(BaseModel, Base):
         @amenities.setter
         def amenities(self, value):
             if type(value) == Amenity:
-                self.amenity_ids.append(value.id)
+		self.amenity_ids.append(value.id)
